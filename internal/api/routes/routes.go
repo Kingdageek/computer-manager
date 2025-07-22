ee044ace33a7ec461d6782121820767f59e14ad5
Nonso Mgbechi <mgbechinonso@gmail.com>
2025-07-23 01:48:03 +0200

    added initial computer management endpoints & dummy controller methods

--- internal/api/routes/routes.go
+++ internal/api/routes/routes.go
@@ -7,5 +7,12 @@ import (
 )
 
 func CreateRoutes(router *gin.Engine, handlers *controllers.Controllers) {
+	v1 := router.Group("/api/v1")
 
+	v1.GET("/computers", handlers.Computer.GetAll)
+	v1.GET("/computers/:id", handlers.Computer.GetByID)
+	v1.POST("/computers", handlers.Computer.Create)
+	v1.PUT("/computers/:id", handlers.Computer.Update)
+	v1.DELETE("/computers/:id", handlers.Computer.Delete)
+	v1.POST("/computers/:id/employees", handlers.Computer.AssignEmployee)
 }
--- internal/controllers/computer_controller.go
+++ internal/controllers/computer_controller.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "computer-manager/internal/services"
+import (
+	"computer-manager/internal/services"
+
+	"github.com/gin-gonic/gin"
+)
 
 type ComputerController struct {
 	svc *services.ComputerService
@@ -10,4 +14,17 @@ func NewComputerController(svcs *services.Services) *ComputerController {
 	return &ComputerController{
 		svc: svcs.ComputerService,
 	}
+}
+
+func (c *ComputerController) GetAll(ginCtx *gin.Context) {}
+
+func (c *ComputerController) GetByID(ginCtx *gin.Context) {}
+
+func (c *ComputerController) Create(ginCtx *gin.Context) {}
+
+func (c *ComputerController) Update(ginCtx *gin.Context) {}
+
+func (c *ComputerController) Delete(ginCtx *gin.Context) {}
+
+func (c *ComputerController) AssignEmployee(ginCtx *gin.Context) {
 }
