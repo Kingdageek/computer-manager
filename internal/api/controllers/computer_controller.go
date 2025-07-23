587189746cf027c253d5cdaf1cbae98a072ed0d2
Nonso Mgbechi <mgbechinonso@gmail.com>
2025-07-23 16:42:02 +0200

    added controller impl of get by ID

--- internal/api/controllers/computer_controller.go
+++ internal/api/controllers/computer_controller.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"computer-manager/internal/api"
+	"computer-manager/internal/api/http_errors"
 	"computer-manager/internal/services"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,6 +20,8 @@ func NewComputerController(svcs *services.Services) *ComputerController {
 }
 
 func (c *ComputerController) GetAll(ginCtx *gin.Context) {
+	// so request can be cancelled if the client disconnects
+	// or if the server is shutting down
 	reqCtx := ginCtx.Request.Context()
 	data, err := c.svc.GetAllComputers(reqCtx)
 	if err != nil {
@@ -28,7 +32,19 @@ func (c *ComputerController) GetAll(ginCtx *gin.Context) {
 }
 
 func (c *ComputerController) GetByID(ginCtx *gin.Context) {
-
+	reqCtx := ginCtx.Request.Context()
+	computerIdStr := ginCtx.Param("id")
+	computerId, err := strconv.ParseUint(computerIdStr, 10, 0)
+	if err != nil {
+		api.ErrorResponse(ginCtx, http_errors.NewBadRequestError("Invalid computer ID"))
+		return
+	}
+	data, err := c.svc.GetComputerByID(reqCtx, uint(computerId))
+	if err != nil {
+		api.ErrorResponse(ginCtx, err)
+		return
+	}
+	api.SuccessResponse(ginCtx, data)
 }
 
 func (c *ComputerController) Create(ginCtx *gin.Context) {}
